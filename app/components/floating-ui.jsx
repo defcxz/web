23a32ea82ca93bf4243d3946eb028d6e844619db
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-01-01 14:09:01 +0000

    Fix subtitle

---
+++ app/components/floating-ui.jsx
@@ -0,0 +1,31 @@
+import { computePosition, shift, flip, offset } from "@floating-ui/dom";
+
+const floating = document.getElementsByClassName("socialDescription");
+
+document.addEventListener("mousemove"), ({clientX, clientY}) => {
+   const virtualEl = {
+      getBoundingClientReact() {
+         return {
+            width: 0,
+            heigth: 0,
+            x: clientX,
+            y: clientY,
+            left: clientX,
+            right: clientX,
+            top: clientY,
+            bottom: clientY
+         };
+      }
+   };
+
+   computePosition(virtualEl, floating, {
+      placement: "right-start",
+      middleware: [offset(5), flip(), shift()]
+   }).then(({ x, y }) => {
+   Object.assign(floating.style, {
+      top: `${y}px`,
+      left: `${x}px`
+      });
+   });
+
+});
