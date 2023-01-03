a5e823c36d76875376e658dc0ed8675f0476ac84
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-01-03 16:35:58 +0000

    Updated CSS

--- app/page.tsx
+++ app/page.tsx
@@ -11,35 +11,33 @@ export default function Home() {
           <img className={styles.medark} src="/IMGs/medark.svg" alt="Mario G." width="60%"/>
 
           <div className={styles.side}>
-            <div className={styles.mobileblur}>
-              <h1 className={styles.title}>
-                Mario G.
-              </h1>
-              <p className={styles.description}>
-                20 años. Programador y Aspirante a Ingeniero en Informática.
-              </p>
-                <hr className={styles.separador}/>
+            <h1 className={styles.title}>
+              Mario G.
+            </h1>
+            <p className={styles.description}>
+              20 años. Programador y Aspirante a Ingeniero en Informática.
+            </p>
+              <hr className={styles.separador}/>
 
-              <div className={styles.icon}>
-                <p className={styles.subtitle}>Puedes encontrarme aquí!</p>
-                <a href="https://www.instagram.com/defcxz">
-                  <img src="IMGs/instagram.svg" alt="instagram-logo"/>
-                  <p className={styles.socialDescription}>De vez en cuando publico aquí</p>
-                </a>
-                <a href="mailto:mralejandrogu@gmail.com">
-                  <img src="IMGs/mail.svg" alt="mail-logo"/>
-                  <p className={styles.socialDescription}>Mi correo, por si gustas contactarme por ahí</p>
-                </a>
-                <a href="https://www.github.com/defcxz">
-                  <img src="IMGs/github.svg" alt="github-logo"/>
-                  <p className={styles.socialDescription}>Aquí tengo algunos proyectos de programación!</p>
-                </a>
-                <a href="https://sptfyy.glitch.me/h4nrb">
-                  <img src="IMGs/spotify.svg" alt="spotify-logo"/>
-                  <p className={styles.socialDescription}>Mis gustos musicales 🎶💃🏻</p>
-                </a>
-            
-              </div>
+            <div className={styles.icon}>
+              <p className={styles.subtitle}>Puedes encontrarme aquí!</p>
+              <a href="https://www.instagram.com/defcxz">
+                <img src="IMGs/instagram.svg" alt="instagram-logo"/>
+                <p className={styles.socialDescription}>De vez en cuando publico aquí</p>
+              </a>
+              <a href="mailto:mralejandrogu@gmail.com">
+                <img src="IMGs/mail.svg" alt="mail-logo"/>
+                <p className={styles.socialDescription}>Mi correo, por si gustas contactarme por ahí</p>
+              </a>
+              <a href="https://www.github.com/defcxz">
+                <img src="IMGs/github.svg" alt="github-logo"/>
+                <p className={styles.socialDescription}>Aquí tengo algunos proyectos de programación!</p>
+              </a>
+              <a href="https://sptfyy.glitch.me/h4nrb">
+                <img src="IMGs/spotify.svg" alt="spotify-logo"/>
+                <p className={styles.socialDescription}>Mis gustos musicales 🎶💃🏻</p>
+              </a>
+          
             </div>
           </div>
 
