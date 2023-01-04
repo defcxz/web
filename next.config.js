cdee569962d6874ecb70079ebac99c7976c1839d
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-01-04 15:28:36 +0000

    Added /drop as a redirection page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/drop',
+        destination: 'https://www.dropbox.com/scl/fo/mv8b1k3mku5wru8vvdyci/h?dl=0&rlkey=jr2837i649r3bxiuis52vfvyn',
+        permanent: false
+      },
+    ]
+  },
 }
-module.exports = nextConfig
 
+module.exports = nextConfig
