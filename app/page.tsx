4747af86681dcc648f3906fb3dd4b69004e37966
Mario Gómez <ma.gomez@duocuc.cl>
2023-04-12 16:12:44 +0000

    deleted unnecesary divs. added new section

--- app/page.tsx
+++ app/page.tsx
@@ -2,48 +2,56 @@ import { Form } from './components/form';
 import styles from './styles/page.module.css';
 import Image from 'next/image';
 import content from './content.json';
+import { FaReact } from 'react-icons/fa';
+import { SiTypescript, SiNextdotjs, SiPlanetscale, SiMysql, SiPython } from 'react-icons/si'
 
 export default function Home() {
   
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <main className={styles.main}>
         
-        <div className={styles.grid}>
+      <div className={styles.grid}>
 
-          {/* <Image src="/IMGs/medark.svg" priority className={styles.medark} alt="Mario G." height={500} width={500}/> */}
-
-          <div className={styles.side}>
-            <h1 className={styles.title}>
-              {content.nombre}
-            </h1>
-            <div className={styles.icon}>
-              <p className={styles.subtitle}>Puedes encontrarme aquí!</p>
-                <a href="https://www.instagram.com/defcxz">
-                  <Image src="/IMGs/instagram.svg" alt="instagram-logo" width={60} height={60}/>
-                  <p className={styles.socialDescription}>De vez en cuando publico aquí</p>
-                </a>
-                <a href="https://www.github.com/defcxz">
-                  <Image src="/IMGs/github.svg" alt="github-logo" width={60} height={60}/>
-                  <p className={styles.socialDescription}>Aquí tengo algunos proyectos de programación!</p>
-                </a>
-                <a href="mailto:ma.gomez@duocuc.cl">
-                  <Image src="/IMGs/mail.svg" alt="mail-logo" width={60} height={60}/>
-                  <p className={styles.socialDescription}>Mi correo, por si gustas contactarme por ahí</p>
-                </a>
-                {/* <a href="https://open.spotify.com/user/lameavollaiiita?si=ce05a56774b043c1">
-                  <Image src="/IMGs/spotify.svg" alt="spotify-logo" width={60} height={60}/>
-                  <p className={styles.socialDescription}>Mis gustos musicales 🎶💃🏻</p>
-                </a> */}
-            </div>
-
-            <p className={styles.description}>
-              {content.desc}
-            </p>
-              {/* <Form/> */}
-          </div>
+        <h1 className={styles.title}>
+          {content.nombre}
+        </h1>
+        <div className={styles.icon}>
+          <p className={styles.subtitle}>Puedes encontrarme aquí!</p>
+            <a href="https://www.instagram.com/defcxz">
+              <Image src="/IMGs/instagram.svg" alt="instagram-logo" width={60} height={60}/>
+              <p className={styles.socialDescription}>De vez en cuando publico aquí</p>
+            </a>
+            <a href="https://www.github.com/defcxz">
+              <Image src="/IMGs/github.svg" alt="github-logo" width={60} height={60}/>
+              <p className={styles.socialDescription}>Aquí tengo algunos proyectos de programación!</p>
+            </a>
+            <a href="mailto:ma.gomez@duocuc.cl">
+              <Image src="/IMGs/mail.svg" alt="mail-logo" width={60} height={60}/>
+              <p className={styles.socialDescription}>Mi correo, por si gustas contactarme por ahí</p>
+            </a>
+            {/* <a href="https://open.spotify.com/user/lameavollaiiita?si=ce05a56774b043c1">
+              <Image src="/IMGs/spotify.svg" alt="spotify-logo" width={60} height={60}/>
+              <p className={styles.socialDescription}>Mis gustos musicales 🎶💃🏻</p>
+            </a> */}
         </div>
+
+        <p className={styles.description}>
+          {content.descripcion}
+        </p>
+      </div>
+        <section className={styles.sobremi}>
+          <h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati iste vero, vitae reprehenderit modi cum. Earum mollitia libero officiis ut ullam laborum exercitationem! Incidunt, modi assumenda. Aspernatur nemo unde sequi!
+          </h2>
+          <div className={styles.icon}>
+            <FaReact />
+            <SiTypescript />
+            <SiNextdotjs />
+            <SiPlanetscale />
+            <SiMysql />
+            <SiPython />
+          </div>
+        </section>
     </main>
-  </div>
   )
 }
