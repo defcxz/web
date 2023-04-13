a38a5aee857d346eb20688c075e1b9a0456a6e27
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-04-13 00:52:15 -0400

    Updated project view

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/projects.module.css'
 import content from './projects.content.json'
+import { BsArrowUpRightCircle } from 'react-icons/bs'
 
 export default function Projects() {
   return (
@@ -8,8 +9,14 @@ export default function Projects() {
       <div className={styles.proyectos}>
          {content.projects.map((project) => (
             <div className={styles.card} key={project.id}>
-               <h2>{project.name}</h2>
                <p>{project.description}</p>
+               <div className={styles.nameCard}>
+                  <h2>{project.name}</h2>
+                  {/* <p className={styles.year}>{project.year}</p> */}
+                  <a href={project.url} target="_blank">
+                     <BsArrowUpRightCircle />
+                  </a>
+               </div>
             </div>
          ))}
       </div>
