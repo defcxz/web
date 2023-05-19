834dcd27eeb88c0215fd4a951dc440ea5c8e189e
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-05-18 21:36:20 -0400

    Security improvements

--- pages/api/utils.tsx
+++ pages/api/utils.tsx
@@ -1,10 +1,13 @@
 import { createClient } from 'contentful';
 import { formatDistanceToNow } from 'date-fns';
 
+const space_id = process.env.CONTENTFUL_SPACE_ID;
+const access_token = process.env.CONTENTFUL_ACCESS_TOKEN;
+
 const client = createClient({
-  space: 'xyjsqq0bqm3o',
+  space: space_id ? space_id : '',
   environment: 'master',
-  accessToken: 'y29Liiu0I_IP6qsRYRLtmn5kuRLfUv9toNSGYgjKuiE'
+  accessToken: access_token ? access_token : '',
 });
 
 // Retrieve the list of posts from Contentful
