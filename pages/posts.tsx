56e7ee93d71e12ddfe896f5a6ad4c2594a8e83bd
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-05-19 00:20:32 -0400

    Keep working on implementing posts

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -4,13 +4,6 @@ import { getBlogEntries } from "./api/utils";
 import styles from '../app/styles/posts.module.css'
 import Link from "next/link";
 
-export const getStaticProps: GetStaticProps = async () => {
-   const posts = await getBlogEntries();
-   return {
-     props: { posts },
-   };
-};
- 
 
 export default function Posts( { posts }) {
    return (
@@ -18,13 +11,22 @@ export default function Posts( { posts }) {
          <h1 className={styles.title}>Posts</h1>
          <p className={styles.subtitle}>Porque sí, también me gusta escribir.</p>
          {posts.map((post) => (
-            <Link href={`/posts/${post.id}`} key={post.id}>
-               <li className={styles.list}>
-                  <h2>{post.title}</h2>
-                  <p className={styles.date}>{post.date}</p>
-               </li> 
+            <Link href={`/posts/${post.slug}`} key={post.id}>
+               <>
+                  <li className={styles.list}>
+                     <h2>{post.title}</h2>
+                     <p className={styles.date}>{post.date}</p>
+                  </li>
+               </>
             </Link>
-         ))}  
+         ))}
       </main>
    );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+   const posts = await getBlogEntries();
+   return {
+     props: { posts },
+   };
+};
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,34 +1,37 @@
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 
-import { GetStaticProps } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import { getBlogEntries } from "../api/utils";
 
 export default function Post({ post }){
 
    const router = useRouter();
 
-   if(!router.isFallback && !post) {
+   if(router.isFallback && !post) {
       return <ErrorPage statusCode={404} />
    }
 
    return (
-   <h1>{post.title}</h1>
+      <h1>{post.title}</h1>
    );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async ({params}) => {
+   const slug = params?.slug; // Obtener el slug de la URL
    const posts = await getBlogEntries();
+   const post = posts.find((post) => post.slug === slug); // Buscar el post por el slug
+   const filteredPosts = posts.filter((post) => post.slug); // Filtrar los posts sin slug
    return {
-     props: { posts },
+     props: { posts: filteredPosts, post },
    };
 };
- 
 
 export async function getStaticPaths() {
    const allPosts = await getBlogEntries();
+   const paths = allPosts.map(({ slug }) => ({ params: { slug }}));
    return {
-     paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
+     paths,
      fallback: true,
    }
 }
