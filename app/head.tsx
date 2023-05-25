1bad1bb0019bb181aa194f962c9a2dae56e846c3
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-05-25 01:10:29 -0400

    Fix

--- app/head.tsx
+++ app/head.tsx
@@ -4,12 +4,14 @@ export default function Head() {
       <title>Inicio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Mi portafolio web." />
+
       <meta property="description" content="Mi portafolio web." />
       <meta property="twitter:image" content="/favicon.ico"></meta>
       <meta property="og:title" content="Mario G."></meta>
       <meta property="og:description" content="Mi portafolio web." />
       <meta property="og:image" content="/favicon.ico"></meta>
       <meta property="og:url" content="https://mgu.vercel.app"></meta>
+      
       <link rel="icon" href="/favicon.ico" />
     </>
   )
