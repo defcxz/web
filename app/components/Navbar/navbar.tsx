01204b0de0fd0fb48a58b95ee64386be26ea7cf7
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2023-06-06 23:32:03 -0400

    fix test

--- app/components/Navbar/navbar.tsx
+++ app/components/Navbar/navbar.tsx
@@ -3,37 +3,29 @@ import styles from './navbar.module.css';
 
 export default function Navigationbar(){
 
-    // const links = [
-    //     {
-    //         title: 'Posts',
-    //         url: '/posts',
-    //     },
-    //     {
-    //         title: 'Resumé',
-    //         url: '/docs/Resume.pdf',
-    //     },
-    // ];
+    const links = [
+        {
+            title: 'Posts',
+            url: '/posts',
+        },
+        {
+            title: 'Resumé',
+            url: '/docs/Resume.pdf',
+        },
+    ];
 
 
     return (
         <nav className={`${styles.nav}`}>
             <ul>
                 <Link className={styles.name} href="/" as="/">Mario G.</Link>
-                <Link className={`nav_item`} href={'/posts'} as={'/posts'}>
-                    Posts
-                </Link>   
-                <Link className={`nav_item`} href={'/docs/Resume.pdf'} as={'/docs/Resume.pdf'}>
-                    Resumé
-                </Link>
-
-                
-                {/* {
-                    links.map((link, index) => (
-                        <Link className={`nav_item ${link.url}`} key={index} href={link.url} as={link.url}>
+                {
+                    links.map((link) => (
+                        <Link className={`nav_item ${link.url}`} href={link.url} as={link.url}>
                             {link.title}
                         </Link>     
                     ))
-                } */}
+                }
             </ul>
         </nav>
     )
