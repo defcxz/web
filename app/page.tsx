4f37d144b2e5cb516f24be3b4e218c016946f820
Mario Gómez <ma.gomez@duocuc.cl>
2023-09-06 00:16:28 +0000

    A lot of design changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
 import styles from './styles/page.module.css';
 import Image from 'next/image';
 import content from './content.json';
+import Projects from './projects/page';
+
 export default function Home() {
+
 
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        <h1 className={`${styles.title}`}>
-          {content.descripcion}
-        </h1>
-
+      <h1 className={`${styles.title}`}>
+        {content.descripcion}
+      </h1>
+      <div className={styles.grouplist}>
         <p className={`${styles.subtitle}`}>
           Social media
         </p>
         <div className={styles.list}>
           {content.socialmedia.map((item) => (
             <div key={item.id}>
-              <a href={item.url} target='_blank' className={`${styles.container} ${styles.glowing}`}>
+              <a href={item.url} target='_blank' className={`${styles.container2} ${styles.glowing}`}>
                 <Image src={item.icon} alt={`${item.name}-logo`} width={60} height={60} />
                 <p className={styles.link}>{item.name}</p>
               </a>
@@ -24,21 +26,8 @@ export default function Home() {
           ))}
         </div>
 
-        <p className={`${styles.subtitle2}`}>
-          Projects
-        </p>
-        <div className={styles.list2}>
-          {content.projects.map((item) => (
-            <div key={item.id}>
-              <a href={item.url} className={`${styles.container2} ${styles.glowing}`}>
-                {/* <Image src={item.icon} alt={`${item.name}-logo`} width={60} height={60} /> */}
-                <Image src='/assets/arrow.svg' alt={`${item.name}-logo`} width={20} height={20} />
-                <p className={styles.link}>{item.name}</p>
-              </a>
-            </div>
-          ))}
-        </div>
-        
+
+        <Projects />
       </div>
     </main>
   )
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,32 +1,52 @@
-import styles from './projects.module.css'
-import { BsArrowUpRightCircle } from 'react-icons/bs'
-import Link from 'next/link'
-import content from './projects.content.json'
+"use client";
 
-
-// async function getProjects () {
-//   const projects = await getProjectEntries();
-//   return projects;
-// }
-
+import styles from './projects.module.css';
+import Image from 'next/image';
+import content from '../content.json';
+import { useState } from 'react';
 
 export default function Projects() {
-  // const project = await getProjects();
+
+  const [openProjectId, setOpenProjectId] = useState(null);
+
+  const toggleProject = (projectId) => {
+    if (openProjectId === projectId) {
+      setOpenProjectId(null);
+    } else {
+      setOpenProjectId(projectId);
+    }
+  };
+
   return (
-   <div className={styles.container}>
-      <h1 className={styles.title}>Proyectos</h1>
-      <div className={styles.proyectos}>
-      {content.projects.map((project, index) => (
-        <div className={`${styles.card} delay-${index + 1}`} key={project.id} style={{ animationDelay: `${(index + 1) * .7}s` }}>
-          <Link href={project.url} target="_blank">
-            <div className={styles.nameCard}>
-              <p>{project.name}</p>
-              <BsArrowUpRightCircle />
+    <main>
+      <p className={`${styles.subtitle}`}>
+        Projects
+      </p>
+      <div className={styles.list}>
+        {content.projects.map((item) => (
+          <div key={item.id}>
+            <div
+              onClick={() => toggleProject(item.id)} // Llama a toggleProject al hacer clic
+              className={`${styles.container2} ${styles.glowing}`}
+            >
+              <Image src='/assets/arrow.svg' alt={`${item.name}-logo`} width={60} height={60} />
+              <p className={styles.link}>{item.name}</p>
             </div>
-          </Link>
-        </div>
+            {openProjectId === item.id && (
+              <div
+                className={styles.projectContent}
+              >
+                <div className={styles.itemYear}>
+                  <p>{item.year}</p>
+                </div>
+                <div className={styles.itemDescription}>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            )}
+          </div>
         ))}
       </div>
-   </div>
+    </main>
   )
 }
