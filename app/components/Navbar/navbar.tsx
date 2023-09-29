2a735458ea5dbb87d3325ab27f31fc1c35aec02a
Mario Gómez <ma.gomez@duocuc.cl>
2023-09-29 02:09:15 +0000

    css terror

--- app/components/Navbar/navbar.tsx
+++ app/components/Navbar/navbar.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
+import logo from '../../../public/assets/mi-logo.svg'
+import { FiGithub } from 'react-icons/fi'
 import Link from 'next/link'
 import styles from './navbar.module.css'
-import logo from '../../../public/assets/mi-logo.svg'
 
 export default function Navbar() {
 
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
+        <li className={styles.brand}>
           <Image
             className={styles.logo}
             src={logo}
@@ -16,6 +17,17 @@ export default function Navbar() {
             height={45}
             alt="Logo"
           />
+        </li>
+        <li className={styles.links}>
+          <Link href="mailto:ma.gomez@duocuc.cl" target='_blank'>
+            Mail
+          </Link>
+          <Link href="https://www.github.com/defcxz" target='_blank'>
+            <FiGithub
+              className={styles.github}
+              size={25}
+            />
+          </Link>
         </li>
       </ul>
     </nav>
